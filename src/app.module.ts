706c4312d86d8c3acff12df412dc7ab5d14bdc2d
Nadav Zeira <nadavzeira@gmail.com>
2022-11-23 15:08:09 +0200

    Add GraphQL & TypeORM modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { StorefrontsModule } from './storefronts/storefronts.module';
+
+const GraphQL = GraphQLModule.forRoot<ApolloDriverConfig>({
+  driver: ApolloDriver,
+  autoSchemaFile: './schema.gql',
+  debug: true,
+  playground: true,
+});
+
+const TypeOrm = TypeOrmModule.forRoot({
+  keepConnectionAlive: true,
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '123456',
+  database: 'reef',
+  autoLoadEntities: true,
+  synchronize: true,
+});
 
 @Module({
-  imports: [],
+  imports: [GraphQL, TypeOrm, StorefrontsModule],
   controllers: [AppController],
   providers: [AppService],
 })
